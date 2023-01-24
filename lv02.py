ef057d4758c5f4ca0ab55e912708c1fa82832194
eric polley <ericpolley@live.com>
2023-01-23 18:00:35 -0800

    ai to py file for reuse. reused at lv2 and lv3

--- lv02.py
+++ lv02.py
@@ -11,29 +11,7 @@ if player.intersects(checkPoint02) and lvl == 2:
     lvl = 3
 
 # ENEMY AI
-distance = math.sqrt((player.x - enemy1.x)**2 + (player.y -
-                                                 enemy1.y)**2 + (player.z - enemy1.z)**2)
-# if enemy1 gets too far on the z axis, add 2 to the z axis.
-if enemy1.z < 11:
-    enemy1.z = enemy1.z + 2
-if distance < 40 and enemy1.z > 10:
-    # Move the enemy1 towards the player
-    enemy1.position = lerp(
-        enemy1.position, player.position, time.dt * .45)
-# enemy collison group
-enemy1.collision_group = "Enemy"
-# Keeps the Enemy off of the floor
-if enemy1.y < ground.y + 1.4:
-    enemy1.y = ground.y + 1.4
-if enemy1.intersects(player):
-    # player.position += enemy1.back * -4
-    player.position = (0, 6, -10)
-    player.rotation = (0, 0, 0)
-
-    if not punchy.playing:
-        punchy.play()
-# look at the player at all times
-player_xz = player.position
-player_xz.y = enemy1.position.y
-enemy1.look_at(player_xz)
+with open('enemy01AI.py', 'r') as file:
+    file_enemy01ai = file.read()
+    exec(file_enemy01ai)
 # ENEMY AI ENDS
--- lv03.py
+++ lv03.py
@@ -5,3 +5,10 @@ global checkPoint02
 
 destroy(checkPoint02)
 destroy(lvl3serporaterWall)
+
+
+# ENEMY AI
+with open('enemy01AI.py', 'r') as file:
+    file_enemy01ai = file.read()
+    exec(file_enemy01ai)
+# ENEMY AI ENDS
